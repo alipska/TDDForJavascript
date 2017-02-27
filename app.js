4928eb3d81f94956d945a86662806080fb317c38
alipska <alipska.rybicka@gmail.com>
2017-02-27 14:15:23 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,3 @@
+hello = function(){
+  return 'World'
+}
---
+++ appSpec.js
@@ -0,0 +1,6 @@
+describe('Testing a Hello World', function() {
+ 
+  it('should say hallo to the World', function() {
+    expect(hello()).toEqual('World');
+  });
+});
